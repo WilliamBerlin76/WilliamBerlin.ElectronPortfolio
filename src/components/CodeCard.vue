c54c5c4b77521ef0be3b9796da8d8d963ec8e710
WilliamBerlin76 <william.berlin76@gmail.com>
2021-04-14 16:40:52 -0500

    displaying codes

---
+++ src/components/CodeCard.vue
@@ -0,0 +1,37 @@
+<template class="card">
+    <div>
+        <p>{{name}}</p>
+        <p>{{price}}</p>
+        <qr-code 
+            :text=price
+            :size=size
+            color="#000000"
+            bg-color="#ffffff" 
+            error-level="L">
+        </qr-code>
+    </div>
+  
+</template>
+ 
+<script>
+
+export default {
+    name: "CodeCard",
+    props: {
+        name: String,
+        price: String
+    },
+    data(){
+        return {
+            size: 200
+        }
+    }
+}
+</script>
+
+<style scoped>
+.card{
+    width:25%
+}
+
+</style>
--- src/components/QrCodes.vue
+++ src/components/QrCodes.vue
@@ -1,27 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <qr-code 
-      text="React is better than Vue"
-      size="300"
-      color="#000000"
-      bg-color="#ffffff" 
-      error-level="L">
-    </qr-code>
+  <div class="codes-container">
+    <div class="name-card">
+      <p>{{myName}}</p>
+      <qr-code 
+        :text=myName
+        size=200
+        color="#000000"
+        bg-color="#ffffff" 
+        error-level="L">
+      </qr-code>
+    </div>
+    <div class="code-cards" >
+      <CodeCard 
+        :name=item.name
+        :price=item.price
+        v-for="item in cryptos" 
+        v-bind:key="item.name"
+      />
+    </div>
+    
   </div>
 </template>
 
 <script>
+
+import Vue from "vue";
+import axios from  "axios";
+import VueAxios from "vue-axios";
+import CodeCard from "./CodeCard";
+Vue.use(axios, VueAxios);
+
+
 export default {
   name: 'QrCodes',
-  props: {
-    msg: String
-  }
+  components: {
+    CodeCard
+  },
+  data(){
+    return {
+      myName: "William Berlin",
+      cryptos: []
+    }
+  },
+  mounted(){
+    // tezos
+    axios.get(`https://data.messari.io/api/v1/assets/tezos/metrics`)
+    .then(res => {
+      this.cryptos.push({
+        name: `Tezos`,
+        price: `$${res.data.data.market_data.price_usd}`
+      });
+      
+    }).catch(err => {
+      console.log(err);
+    });
+
+    // burst
+    axios.get(`https://data.messari.io/api/v1/assets/burst/metrics`)
+    .then(res => {
+      this.cryptos.push({
+        name: `Burst`,
+        price: `$${res.data.data.market_data.price_usd}`
+      });
+      
+    }).catch(err => {
+      console.log(err);
+    });
+    // cardano
+    axios.get(`https://data.messari.io/api/v1/assets/cardano/metrics`)
+    .then(res => {
+      this.cryptos.push({
+        name: `Cardano`,
+        price: `$${res.data.data.market_data.price_usd}`
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.code-cards{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.name-card{
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+}
 h3 {
   margin: 40px 0 0;
 }
